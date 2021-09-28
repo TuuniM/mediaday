bd531b2931c57676624b1c1742a00f5ba0d99dc3
Tuuni <tuuni.mustonen@metropolia.fi>
2021-09-28 10:59:17 +0300

    striimitoimii

--- src/pages/EventPage.js
+++ src/pages/EventPage.js
@@ -19,10 +19,6 @@ const EventPage = () => {
 
     return (
         <div>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/qYDsS4jvVMk?controls=0"
-                    title="YouTube video player" frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen></iframe>
 
             <Container>
                 <Row>
